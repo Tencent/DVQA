0ef060aef61866d9581d1d70b990fdff5aaa3584
Haiqiang Wang <tommyhqwang@tencent.com>
2020-04-26 11:31:21 +0800

    support tb add_figure

--- tool/draw.py
+++ tool/draw.py
@@ -3,15 +3,14 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-def mos_scatter(mos, pred, dataset_name):
-    plt.scatter(mos, pred, alpha=0.6)
+def mos_scatter(mos, pred):
+    
+    fig = plt.figure(figsize=(48,48))
+    plt.scatter(mos, pred, alpha=0.8)
     plt.xlabel('MOS')
     plt.ylabel('PRED')
-    plt.title(dataset_name)
     plt.plot([0, 100], [0, 100])
-    plt.xlim(0, 100)
-    plt.ylim(0, 100)
-    plt.savefig('{}.png'.format(dataset_name), dpi=1000)
 
+    return fig
 
 
